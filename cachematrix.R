1550b58c0a10ab6461b6f295df8f2ce8994c3386
Carmiss18 <63974713+Carmiss18@users.noreply.github.com>
2020-09-27 15:08:19 -0500

    Added the functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,92 +1,55 @@
 ## Assignment: Caching the Inverse of a Matrix
 
 
-## Write a short comment describing this function
+## TThis function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function( m = matrix() ) {
   
   ## Initialize the inverse property
-  i <- NULL
+  inverse <- NULL
   
-  ## Method to set the matrix
-  set <- function( matrix ) {
+   set <- function( matrix ) {
     m <<- matrix
-    i <<- NULL
+    inverse <<- NULL
   }
   
-  ## Method the get the matrix
-  get <- function() {
-    ## Return the matrix
-    m
+   get <- function() {
+     m
   }
   
-  ## Method to set the inverse of the matrix
-  setInverse <- function(inverse) {
-    i <<- inverse
+   setInverse <- function(inverse) {
+    inverse <<- inverse
   }
   
-  ## Method to get the inverse of the matrix
-  getInverse <- function() {
-    ## Return the inverse property
-    i
+   getInverse <- function() {
+       inverse
   }
-  
-  ## Return a list of the methods
+ 
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
-## Write a short comment describing this function
-
+## his function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
   m <- x$getInverse()
   
-  ## Just return the inverse if its already set
+ 
   if( !is.null(m) ) {
     message("getting cached data")
     return(m)
   }
   
-  ## Get the matrix from our object
+ 
   data <- x$get()
   
-  ## Calculate the inverse using matrix multiplication
+ 
   m <- solve(data) %*% data
   
-  ## Set the inverse to the object
+
   x$setInverse(m)
   
-  ## Return the matrix
+ 
   m
 }
-
-
-
-
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-mvec <- makeVector()
-x <- 1:4
-mvec$set(x)
-mvec$get()
-
-mvec$getmean()
-
-mvec$setmean()
-mvec$getmean()
-
